31d9e7e73b3bf40ef5fc3ad6b4758674f4d2762b
Gaurang-gupta <g12172001@gmail.com>
2024-12-07 12:07:11 +0530

    Error solving 1

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import { Link, useSearchParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Card = ({ image, title, desc, author, source, published_at }: {
   image: string,
--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,12 +1,13 @@
-import { FormEvent, useEffect, useState } from "react"
-import data from "../../testData.json"
+import { useEffect, useState } from "react"
+// import data from "../../testData.json"
 import Card from "../Card/Card"
 function Page() {
-    const [search, setSearch] = useState("")
+    // const [search, setSearch] = useState("")
     const [test, setTest] = useState([])
     useEffect(() => {
         const getData = async () => {7
-            const dataFromApi = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`).then(res => res.json()).then(data => setTest(data?.articles))
+            await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`).then(res => res.json()).then(data => setTest(data?.articles))
+            
         }
         getData()
         console.log(test)
@@ -26,7 +27,7 @@ function Page() {
             <button type="submit" className='text-gray-400'>Search</button>
         </form>
 
-        {search !== null && search !== "" && <h1 className="w-[90%] mx-auto pb-3 text-3xl mb-4">Search results for <span className="font-bold uppercase underline underline-offset-4 decoration-6 decoration-orange-400">{search}</span></h1>}
+        {/* {search !== null && search !== "" && <h1 className="w-[90%] mx-auto pb-3 text-3xl mb-4">Search results for <span className="font-bold uppercase underline underline-offset-4 decoration-6 decoration-orange-400">{search}</span></h1>} */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 w-[90%] mx-auto pb-5">
             {test?.map((dat: { urlToImage: any; title: any; description: any; author: any; source: { name: any }; publishedAt: any }, index: any) => (
                 <Card 
